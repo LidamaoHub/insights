ba893de0f43134b21af19615e23fcd48b0e10e37
Tiyo <tiyosheng@gmail.com>
2023-09-09 00:14:35 +0800

    fixed some bugs

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -3,29 +3,27 @@ import { heading, panel, text, copyable, divider } from '@metamask/snaps-ui';
 import { ethers } from 'ethers';
 import { getContractInfo, setShortUrl, getRiskListFun, getSourceCode } from './utils/utils.js';
 
-// Handle outgoing transactions.
 export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
   console.log(transaction)
-  let info = {chain: {}, token: '', to: transaction.to, isProxy: false, isOpenSources: false, riskList: []}
+  let info = { chain: {}, token: '', to: transaction.to, isProxy: false, isOpenSources: false, riskList: [] }
   let isTransaction = true
   if (transaction.data) {
     isTransaction = false
     let p = await new ethers.providers.Web3Provider(window.ethereum, "any");
-    info = {chain: {}, token: '', to: transaction.to, isProxy: false, isOpenSources: false, riskList: []}
+    info = { chain: {}, token: '', to: transaction.to, isProxy: false, isOpenSources: false, riskList: [] }
     let network = await p.getNetwork()
     info.chain = network
-    // info = await setShortUrl(info)
     info = await getContractInfo(info, p)
     if (info.isProxy) {
-      info.riskList.push({risk: true, text: 'upgradeable contract'})
+      info.riskList.push({ risk: true, text: 'upgradeable contract' })
     }
     info = await getSourceCode(info)
     if (!info.isOpenSources) {
-      info.riskList.push({risk: true, text: 'not opensourced'})
+      info.riskList.push({ risk: true, text: 'not opensourced' })
     }
     info = await getRiskListFun(info)
   }
-  
+
   console.log(info, isTransaction)
   return {
     content: panel(isTransaction ? [
